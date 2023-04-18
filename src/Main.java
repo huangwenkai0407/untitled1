6f8d9541cbd0ce7ffb4bfb7b9a882fb3ed340cd0
hwk <891802887@qq.com>
2023-04-18 21:57:28 +0800

    123

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    boolean cmp(int a,int b){
+        return a<b;
+    }
+    public static void main(String[] args) {
+        Map<String,Integer> map=new HashMap<>();
+
+        map.put("223",1);
+        map.put("123",12);
+        map.forEach((key,value)->{
+            System.out.println(key+" "+value);
+
+        });
+    }
+}
